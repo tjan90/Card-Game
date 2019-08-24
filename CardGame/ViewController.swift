bece1b64b887ab461a97a8b6d82cd033bfc93d1c
Tanveer Jan <tjan90@yahoo.com>
2019-08-24 13:33:48 +0200

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Tanveer Jan on 24/08/2019.
+//  Copyright © 2019 Tanveer Jan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
